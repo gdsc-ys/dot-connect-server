b47bb4455c4ef498005098cba61e20a2d15aaf51
inshining <inshining.kim@gmail.com>
2023-02-25 01:11:49 +0900

    🍀[FEAT] : 파일 업로드

--- dot-connect/app.go
+++ dot-connect/app.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+	r.POST("/upload", func(c *gin.Context) {
+		location := c.PostForm("location")
+		content := c.PostForm("content")
+
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+
+		filename := filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, filename); err != nil {
+			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+			return
+		}
+
+		c.String(http.StatusOK, "upload file success: %s, %s, %s", location, content, file.Filename)
 	})
-	r.Run() // listen and serve on
+	r.Run(":8080") // listen and serve on
 }
